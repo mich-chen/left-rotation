024eb7f21f584cfb9d2308fad1d5d2165d6d94ff
Michelle Chen <mich.chen.94@gmail.com>
2020-10-05 11:14:24 -0700

    solution using list slicing

--- left_rotation.py
+++ left_rotation.py
@@ -1,16 +1,19 @@
 def rotLeft(a, d):
-    # instantiate a new list with same length as input
-    rotated = [None] * len(a)
+    # # instantiate a new list with same length as input
+    # rotated = [None] * len(a)
 
-    for i in range(len(a)):
-        # new index caluclated by subtracting num of rotations by length of array
-        # then add current index and modulo length of array for final index
-        rotated[(i + (len(a) - d)) % len(a)] = a[i]
+    # for i in range(len(a)):
+    #     # new index caluclated by subtracting num of rotations by length of array
+    #     # then add current index and modulo length of array for final index
+    #     rotated[(i + (len(a) - d)) % len(a)] = a[i]
 
-    return rotated
+    # return rotated
+
+    # Alternative solution: List Slicing
+    return a[d:] + a[:d]
 
 
 if __name__ == '__main__':
     
-    print(rotLeft([1, 2, 3, 4, 5], 4))
-    print(rotLeft([1, 2, 3, 4, 5], 7))
+    print(rotLeft([1, 2, 3, 4, 5], 4))  # [5, 1, 2, 3, 4]
+    print(rotLeft([1, 2, 3, 4, 5], 7))  # [3, 4, 5, 1, 2]
